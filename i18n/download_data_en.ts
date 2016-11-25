8d456f2f70e238793099cde1426b9a2e53078c39
Jachym Cepicky <jachym.cepicky@gmail.com>
2016-11-25 18:30:57 +0100

    translations

--- i18n/download_data_en.ts
+++ i18n/download_data_en.ts
@@ -1,33 +1,37 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<!DOCTYPE TS><TS version="1.1" language="en_US">
 <context>
     <name>DownloadDataDialog</name>
     <message>
+        <location filename="../download_data_dialog.py" line="89"/>
         <source>Save Outup as *.shp</source>
-        <translation type="obsolete">Save as *.shp</translation>
+        <translation type="unfinished">Save as *.shp</translation>
     </message>
     <message>
+        <location filename="../download_data_dialog.py" line="92"/>
         <source>shapefile (*.shp)</source>
-        <translation type="obsolete">ESRI Shapefile (*.shp)</translation>
+        <translation type="unfinished">ESRI Shapefile (*.shp)</translation>
     </message>
     <message>
-        <source>NepodaÅilo se naÄÃ­t nÄkterÃ© dÅ¯leÅ¾itÃ© moduly</source>
-        <translation type="obsolete">Some important modules could not be found</translation>
+        <location filename="../download_data_dialog.py" line="54"/>
+        <source>Nepoda&#xc5;&#x99;ilo se na&#xc4;&#x8d;&#xc3;&#xad;t n&#xc4;&#x9b;kter&#xc3;&#xa9; d&#xc5;&#xaf;le&#xc5;&#xbe;it&#xc3;&#xa9; moduly</source>
+        <translation type="unfinished">Some important modules could not be found</translation>
     </message>
     <message>
-        <source>Chyba pÅi importu modulÅ¯</source>
-        <translation type="obsolete">Error while loading modules</translation>
+        <location filename="../download_data_dialog.py" line="55"/>
+        <source>Chyba p&#xc5;&#x99;i importu modul&#xc5;&#xaf;</source>
+        <translation type="unfinished">Error while loading modules</translation>
     </message>
     <message>
-        <source>BohuÅ¾el se nepodaÅilo naÄÃ­st nÄkterÃ© dÅ¯leÅ¾itÃ© moduly:
+        <location filename="../download_data_dialog.py" line="61"/>
+        <source>Bohu&#xc5;&#xbe;el se nepoda&#xc5;&#x99;ilo na&#xc4;&#x8d;&#xc3;&#xad;st n&#xc4;&#x9b;kter&#xc3;&#xa9; d&#xc5;&#xaf;le&#xc5;&#xbe;it&#xc3;&#xa9; moduly:
 
   {}
 
-ZÅejmÄ je nemÃ¡te nainstalovanÃ© ve vaÅ¡em systÃ©mu.
+Z&#xc5;&#x99;ejm&#xc4;&#x9b; je nem&#xc3;&#xa1;te nainstalovan&#xc3;&#xa9; ve va&#xc5;&#xa1;em syst&#xc3;&#xa9;mu.
 
-NÃ¡vod na instalaci chybÄjÃ­cÃ­ch modulÅ¯ mÅ¯Å¾ete nalÃ©zt nÃ­Å¾e:</source>
-        <translation type="obsolete">Some important modules could not be loaded:
+N&#xc3;&#xa1;vod na instalaci chyb&#xc4;&#x9b;j&#xc3;&#xad;c&#xc3;&#xad;ch modul&#xc5;&#xaf; m&#xc5;&#xaf;&#xc5;&#xbe;ete nal&#xc3;&#xa9;zt n&#xc3;&#xad;&#xc5;&#xbe;e:</source>
+        <translation type="unfinished">Some important modules could not be loaded:
 
 {}
 
@@ -36,83 +40,116 @@ It seems, they are not availabe in your system.
 Installation manual for missing modules can be found lower:</translation>
     </message>
     <message>
-        <source>Pro instalaci chybÄjÃ­ch modulÅ¯ spusÅ¥te pÅÃ­kazovou ÅÃ¡dku Windows (cmd) a v nÃ­ spusÅ¥te nÃ¡sledujÃ­cÃ­ pÅÃ­kaz: 
+        <location filename="../download_data_dialog.py" line="69"/>
+        <source>Pro instalaci chyb&#xc4;&#x9b;j&#xc3;&#xad;ch modul&#xc5;&#xaf; spus&#xc5;&#xa5;te p&#xc5;&#x99;&#xc3;&#xad;kazovou &#xc5;&#x99;&#xc3;&#xa1;dku Windows (cmd) a v n&#xc3;&#xad; spus&#xc5;&#xa5;te n&#xc3;&#xa1;sleduj&#xc3;&#xad;c&#xc3;&#xad; p&#xc5;&#x99;&#xc3;&#xad;kaz: 
 
 </source>
-        <translation type="obsolete">For Installation of missing modules open Windows Command Line (cmd) and run following command in there:
+        <translation type="unfinished">For Installation of missing modules open Windows Command Line (cmd) and run following command in there:
 
 </translation>
     </message>
     <message>
-        <source>Pro instalaci chybÄjÃ­ch modulÅ¯ spusÅ¥te pÅÃ­kazovou ÅÃ¡dku a pouÅ¾ijte pÅÃ­kaz: 
+        <location filename="../download_data_dialog.py" line="73"/>
+        <source>Pro instalaci chyb&#xc4;&#x9b;j&#xc3;&#xad;ch modul&#xc5;&#xaf; spus&#xc5;&#xa5;te p&#xc5;&#x99;&#xc3;&#xad;kazovou &#xc5;&#x99;&#xc3;&#xa1;dku a pou&#xc5;&#xbe;ijte p&#xc5;&#x99;&#xc3;&#xad;kaz: 
 
 </source>
-        <translation type="obsolete">For installation of missing modules open terminal and run command:
+        <translation type="unfinished">For installation of missing modules open terminal and run command:
 
 </translation>
     </message>
 </context>
 <context>
     <name>htableDialogBase</name>
-    <message utf8="true">
+    <message encoding="UTF-8">
         <location filename="../download_data_dialog_base.ui" line="14"/>
         <source>Stáhni data z MA CLEERIO</source>
         <translation>Download data from MA CLEERIO</translation>
     </message>
-    <message utf8="true">
+    <message encoding="UTF-8">
         <location filename="../download_data_dialog_base.ui" line="34"/>
         <source>Doména:</source>
         <translation>Domain:</translation>
     </message>
-    <message utf8="true">
-        <location filename="../download_data_dialog_base.ui" line="92"/>
+    <message encoding="UTF-8">
+        <location filename="../download_data_dialog_base.ui" line="95"/>
         <source>Název MA:</source>
         <translation>MA Name:</translation>
     </message>
-    <message utf8="true">
-        <location filename="../download_data_dialog_base.ui" line="114"/>
+    <message encoding="UTF-8">
+        <location filename="../download_data_dialog_base.ui" line="120"/>
         <source>bez přihlášení</source>
         <translation>Do not login</translation>
     </message>
-    <message utf8="true">
-        <location filename="../download_data_dialog_base.ui" line="129"/>
+    <message encoding="UTF-8">
+        <location filename="../download_data_dialog_base.ui" line="135"/>
         <source>Jméno:</source>
         <translation>Name:</translation>
     </message>
     <message>
-        <location filename="../download_data_dialog_base.ui" line="153"/>
+        <location filename="../download_data_dialog_base.ui" line="159"/>
         <source> Heslo: </source>
         <translation>Password:</translation>
     </message>
-    <message utf8="true">
+    <message encoding="UTF-8">
         <location filename="../download_data_dialog_base.ui" line="190"/>
         <source>Dostupná data (získání trvá pár vteřin)</source>
-        <translation>Available data (it can take several seconds)</translation>
+        <translation type="obsolete">Available data (it can take several seconds)</translation>
     </message>
-    <message utf8="true">
+    <message encoding="UTF-8">
         <location filename="../download_data_dialog_base.ui" line="199"/>
         <source>Dosupné:</source>
-        <translation>Available datasets:</translation>
+        <translation type="obsolete">Available datasets:</translation>
     </message>
-    <message utf8="true">
-        <location filename="../download_data_dialog_base.ui" line="222"/>
+    <message encoding="UTF-8">
+        <location filename="../download_data_dialog_base.ui" line="235"/>
         <source>Výstup:</source>
         <translation>Output file:</translation>
     </message>
     <message>
-        <location filename="../download_data_dialog_base.ui" line="280"/>
+        <location filename="../download_data_dialog_base.ui" line="293"/>
         <source>...</source>
         <translation>...</translation>
     </message>
-    <message utf8="true">
-        <location filename="../download_data_dialog_base.ui" line="302"/>
+    <message encoding="UTF-8">
+        <location filename="../download_data_dialog_base.ui" line="315"/>
         <source>Stáhnout  obrázky (pokud jsou)</source>
         <translation>Download images (if available)</translation>
     </message>
-    <message utf8="true">
-        <location filename="../download_data_dialog_base.ui" line="312"/>
+    <message encoding="UTF-8">
+        <location filename="../download_data_dialog_base.ui" line="325"/>
         <source>stáhnout dokumenty (pokud jsou)</source>
         <translation>Download documents (if available)</translation>
+    </message>
+    <message>
+        <location filename="../download_data_dialog_base.ui" line="71"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Cleerio domain name&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Pick Cleerio domain name, you would like to download data from&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../download_data_dialog_base.ui" line="108"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Map Application name&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Type name of map application, you need to list layers from.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../download_data_dialog_base.ui" line="196"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Connect to map application and get list of available data layers&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../download_data_dialog_base.ui" line="199"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../download_data_dialog_base.ui" line="208"/>
+        <source>Available
+layers:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../download_data_dialog_base.ui" line="216"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;List of available layers&lt;/span&gt;&lt;/p&gt;&lt;p&gt;Pick one layer from available layers you wish to download data from.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
